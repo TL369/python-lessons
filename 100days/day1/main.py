4ae936fe27ba1b8f7fdc2ae665e75d72dfef3c55
yuhtruong <yuhtruong1995@gmail.com>
2022-11-21 15:15:45 +0700

    finish day1

--- 100days/day1/main.py
+++ 100days/day1/main.py
@@ -1,1 +1,11 @@
-print('Hello World')
+#1. Create agreeting for your program.
+print('Welcome to the band Name Generator.')
+
+#2. Ask the user for the city that they grew up in.
+city = input("What's name of the city your grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + ' ' + pet)
